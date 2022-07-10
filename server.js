3004da5334b95613d31fb5e2ad9619c245c4509e
Jean Lee <jeanlee.jingli@icloud.com>
2022-07-10 12:12:38 -0700

    add new content in project

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express');
 // Import express-handlebars
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+// const hbs = exphbs.create({helpers});
 const path = require('path');
 
+const routes = require('./controllers');
+// const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 // Sets up the Express App
 const app = express();
@@ -17,8 +21,8 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/home-routes'));
 
+app.use(routes);
   
-
 // Starts the server to begin listening
 app.listen(PORT, () => {
     console.log('Server listening on: http://localhost:' + PORT);
