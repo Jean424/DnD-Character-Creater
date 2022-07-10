49b64ad3df6b4bdede310df8f97e24e9f6a8dba4
Jean Lee <jeanlee.jingli@icloud.com>
2022-07-10 14:30:09 -0700

    add expree-session in package.json and server.js, update .login-form and .signup-form into #login-sorm and #signup-form

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // Dependencies
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
 // Import express-handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-// const hbs = exphbs.create({helpers});
-const path = require('path');
-
 const routes = require('./controllers');
-// const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
 // Sets up the Express App
 const app = express();
@@ -18,6 +21,9 @@ const PORT = process.env.PORT || 3001;
 // The following two lines of code are setting Handlebars.js as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/home-routes'));
 
