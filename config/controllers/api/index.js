e828535fbb97faf4354ad61adec2e42a104066bb
HeartSleeves <97405266+HeartSleeves@users.noreply.github.com>
2022-07-16 15:26:56 -0700

    Fixing branch

--- config/controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const userRoutes = require("./user-routes");
-const characterRoutes = require("./character_routes");
-
-router.use("/users", userRoutes);
-router.use("/characters", characterRoutes);
-
-module.exports = router;
