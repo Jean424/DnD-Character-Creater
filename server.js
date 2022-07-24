fb6596260df5e07bc14da622a64013117df3a17a
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-07-23 20:48:05 -0700

    deploying app

--- server.js
+++ server.js
@@ -58,20 +58,6 @@ app.use(routes);
 
 
 
-//------------------------------------------------------------------------------
-//-- Create Database Connection
-
-// turn on connection to db and server
-/* 
-    - { force: true } == database connection must sync with the model definitions and
-     associations.
-    
-     - By forcing the sync method to true, we will make the tables re-create if
-    there are any association changes.
-
- */
-
-//-- use xisting tables if exist, start connection to express and SQL
 sequelize.sync({ force: false }).then(() => {
 //-- Overvwrite existing tables if exist, start connection to express and SQL
 // sequelize.sync({ force: true }).then(() => {
