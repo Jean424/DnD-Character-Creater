3a01a59cb60d877a4ab03a49607ac8c5ad28f1e3
HeartSleeves <97405266+HeartSleeves@users.noreply.github.com>
2022-07-29 11:14:48 -0700

    fixing for heroku

--- config/connection.js
+++ config/connection.js
@@ -5,35 +5,35 @@ let sequelize;
 
 const db = {};
 
-const DB = "users";
-const USER = "user";
-const PASSWORD = "password";
-const HOST = "host";
-const DIALECT = "postgres";
-const PORT = 5432;
+// const DB = "users";
+// const USER = "user";
+// const PASSWORD = "password";
+// const HOST = "host";
+// const DIALECT = "postgres";
+// const PORT = 5432;
 
-sequelize = new Sequelize(DB, USER, PASSWORD, {
-  host: HOST,
-  dialect: DIALECT,
-  port: PORT,
-});
-
-// if (process.env.JAWSDB_URL) {
-// sequelize = new Sequelize("database", "username", "password", {
-//   host: "localhost",
-//   dialect: "mysql",
+// sequelize = new Sequelize(DB, USER, PASSWORD, {
+//   host: HOST,
+//   dialect: DIALECT,
+//   port: PORT,
 // });
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: "localhost",
-//       dialect: "mysql",
-//       port: 3306,
-//     }
-//   );
-// }
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize("database", "username", "password", {
+    host: "localhost",
+    dialect: "mysql",
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
