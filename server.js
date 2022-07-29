07fb9fdc8a285ec2b1ec96447f04cb3203926e4f
HeartSleeves <97405266+HeartSleeves@users.noreply.github.com>
2022-07-29 11:42:41 -0700

    set jawsdb url

--- server.js
+++ server.js
@@ -74,6 +74,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(port, () => console.log("Now listening"));
+  app.listen(port, () => console.log("Now listening on " + port));
 });
 //testing//
