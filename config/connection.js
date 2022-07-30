5990875a42dbf94ab7d0ccedbac0e56ebd7771c6
Jawid Noori <Hajikaka4030@gmail.com>
2022-07-30 02:25:43 -0700

    connection fixed for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,43 +1,21 @@
-// const Sequelize = require("sequelize");
-// require("dotenv").config();
-
-// let sequelize;
-
-// const db = {};
-
-// const DB = "users";
-// const USER = "user";
-// const PASSWORD = "password";
-// const HOST = "host";
-// const DIALECT = "postgres";
-// const PORT = 5432;
-
-// sequelize = new Sequelize(DB, USER, PASSWORD, {
-//   host: HOST,
-//   dialect: DIALECT,
-//   port: PORT,
-// });
-
-// module.exports = sequelize;
-
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
+const db = {};
+
+const DB = "users";
+const USER = "user";
+const PASSWORD = "password";
+const HOST = "host";
+const DIALECT = "postgres";
+const PORT = 5432;
+
+sequelize = new Sequelize(DB, USER, PASSWORD, {
+  host: HOST,
+  dialect: DIALECT,
+  port: PORT,
+});
 
 module.exports = sequelize;
